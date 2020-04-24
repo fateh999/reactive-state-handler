bfe800a9b86dd2a8cff1cc64fb2fd76d22f0fed9
Fateh Farooqui <fateh@Fatehs-MacBook-Pro.local>
2020-04-24 10:31:05 +0530

    First Commit

---
+++ src/ReactiveState.js
@@ -0,0 +1,28 @@
+import { BehaviorSubject } from "rxjs";
+class ReactiveState {
+  constructor(initialState) {
+    this.initialState = initialState;
+    this.subscriber$ = new BehaviorSubject(initialState);
+  }
+  setValue(keyName, value) {
+    const currentState = this.subscriber$.getValue();
+    this.subscriber$.next({}, ...currentState, [keyName], value);
+  }
+
+  getValue(keyName) {
+    const currentState = this.subscriber$.getValue();
+    return currentState[keyName];
+  }
+
+  setState(values) {
+    const currentState = this.subscriber$.getValue();
+    this.subscriber$.next({}, ...currentState, ...values);
+  }
+  getState() {
+    return this.subscriber$.getValue();
+  }
+  resetState() {
+    this.subscriber$.next(this.initialState);
+  }
+}
+export default ReactiveState;
